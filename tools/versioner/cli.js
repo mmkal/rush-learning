75a1fca3c6900362be998b5aff91cc9e0f4e57b1
mmkal <misha.kaletsky@gmail.com>
2020-09-14 13:02:08 -0400

    versioner

--- tools/versioner/cli.js
+++ tools/versioner/cli.js
@@ -1,13 +1,27 @@
-const standardVersion = require('standard-version')
+// const standardVersion = require('standard-version')
 
-// Options are the same as command line, except camelCase
-// standardVersion returns a Promise
-standardVersion({
-  noVerify: true,
-  // infile: 'docs/CHANGELOG.md',
-  silent: false
-}).then(() => {
-  // standard-version is done
-}).catch(err => {
-    console.error(`standard-version failed with message: ${err.message}`)
+// // Options are the same as command line, except camelCase
+// // standardVersion returns a Promise
+// standardVersion({
+//   noVerify: true,
+//   // infile: 'docs/CHANGELOG.md',
+//   silent: false
+// }).then(() => {
+//   // standard-version is done
+// }).catch(err => {
+//     console.error(`standard-version failed with message: ${err.message}`)
+// })
+const gitRawCommits = require('git-raw-commits')
+const conventionalCommitsParser = require('conventional-commits-parser')
+const concat = require('concat-stream')
+
+gitRawCommits({
+  format: '%B%n-hash-%n%H',
+  from: 'origin/main' || '',
+  // path: options.path
 })
+  .pipe(conventionalCommitsParser())
+  .pipe(concat(data => {
+    console.log({data})
+  }))
+  
