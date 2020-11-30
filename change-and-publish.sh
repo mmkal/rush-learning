986cef052372ae5cef29e71939bc6ec8528137a2
mmkal <misha.kaletsky@gmail.com>
2020-11-30 11:20:36 -0500

    add bulk flag

--- change-and-publish.sh
+++ change-and-publish.sh
@@ -4,4 +4,4 @@ echo "created branch. Delete with:"
 echo "git checkout main && git branch -D $branch_name"
 echo "// comment from $branch_name" >> packages/pkg-a/index.js
 git commit -am "Added comment from $branch_name"
-rush change --bump-type patch --message "$branch_name"
+rush change --bulk --bump-type patch --message "$branch_name"
