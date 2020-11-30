02a7d0b8ccfec284ccd037ff04784ee46c3cb86f
mmkal <misha.kaletsky@gmail.com>
2020-11-30 11:16:05 -0500

    add change-and-publish

---
+++ change-and-publish.sh
@@ -0,0 +1,7 @@
+branch_name="test-change-$(date '+%Y-%m-%d-%H-%M-%S')"
+git checkout -b $branch_name
+echo "created branch. Delete with:"
+echo "git checkout main && git branch -D $branch_name"
+echo "// comment from $branch_name" >> packages/pkg-a/index.js
+git commit -am "Added comment from $branch_name"
+rush change --bump-type patch --message "$branch_name"
